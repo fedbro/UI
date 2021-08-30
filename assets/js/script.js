4c64279ee2997086d0f5e9ed345c70b2f1d72c71
febyghaarvit <59464965+fedbro@users.noreply.github.com>
2021-08-30 10:03:02 +0530

    latest

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const chatbox = jQuery.noConflict();
+
+chatbox(() => {
+  chatbox(".chatbox-open").click(() =>
+    chatbox(".chatbox-popup, .chatbox-close").fadeIn()
+  );
+
+  chatbox(".chatbox-close").click(() =>
+    chatbox(".chatbox-popup, .chatbox-close").fadeOut()
+  );
+
+});
